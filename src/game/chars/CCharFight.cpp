ee5efa6ec1f80fdb12bb503e0165c78d54607e85
Jhobean <frederic.gag@gmail.com>
2023-03-14 14:55:13 -0400

    Changer l'influence du STR de 10% a 2%

--- src/game/chars/CCharFight.cpp
+++ src/game/chars/CCharFight.cpp
@@ -1133,7 +1133,7 @@ int CChar::Fight_CalcDamage( const CItem * pWeapon, bool bNoRandom, bool bGetMax
 				if ( !iStatBonus )
 					iStatBonus = STAT_STR;
 				if ( !iStatBonusPercent )
-					iStatBonusPercent = 10;
+					iStatBonusPercent = 2; //Original c'est 10
 				iDmgBonus += Stat_GetAdjusted(iStatBonus) * iStatBonusPercent / 100;
 				break;
 			}
